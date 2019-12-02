28aedbde894a736b686df0705c0b07c99c2e753b
flabbet <flubbet@gmail.com>
2019-12-02 20:03:41 +0100

    helloworld upload

---
+++ hello-rust/target/debug/deps/hello_rust-67dc167552e2d6cb.d
@@ -0,0 +1,5 @@
+/home/flabbet/Git/gci19-rust-helloworld/hello-rust/target/debug/deps/hello_rust-67dc167552e2d6cb: src/main.rs
+
+/home/flabbet/Git/gci19-rust-helloworld/hello-rust/target/debug/deps/hello_rust-67dc167552e2d6cb.d: src/main.rs
+
+src/main.rs:
---
+++ hello-rust/target/debug/hello-rust.d
@@ -0,0 +1,1 @@
+/home/flabbet/Git/gci19-rust-helloworld/hello-rust/target/debug/hello-rust: /home/flabbet/Git/gci19-rust-helloworld/hello-rust/src/main.rs
